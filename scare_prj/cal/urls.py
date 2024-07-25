404c329487210d0ddce8294f81dffc1f657fc515
bea816 <kimnadon@naver.com>
2024-07-25 18:27:03 +0900

    홈화면2

--- scare_prj/cal/urls.py
+++ scare_prj/cal/urls.py
@@ -6,4 +6,5 @@ app_name = 'cal'
 urlpatterns = [
     path('home/', home, name = "home"),
     path('home/<int:year>/<int:month>/', home, name='home_with_date'),
+    path('home2/<int:year>/<int:month>/<int:day>/', home2, name='home2'),
 ]
--- scare_prj/cal/views.py
+++ scare_prj/cal/views.py
@@ -46,3 +46,50 @@ def home(request, year=None, month=None):
     }
 
     return render(request, 'cal/home.html', context)
+
+def home2(request, year, month, day):
+    selected_day = day
+
+    cal = calendar.Calendar(firstweekday=6)
+    month_days = cal.monthdayscalendar(year, month) # 이번 달 날짜 주별로 리스트
+    
+    # 이전 달의 년, 월을 계산
+    prev_year, prev_month = (year, month - 1) if month > 1 else (year - 1, 12)
+    prev_month_days = calendar.monthrange(prev_year, prev_month)[1] # 이전 달 날짜 수
+
+    # 다음 달의 년, 월을 계산
+    next_year, next_month = (year, month + 1) if month < 12 else (year + 1, 1)
+    
+    # 날짜 수정
+    for i, week in enumerate(month_days):
+        box = 1
+        for j, day in enumerate(week):
+            if day == 0:
+                if i == 0:  # 첫 번째 주
+                    month_days[i][j] = (prev_month_days - week.count(0) + 1, 'other-month')
+                elif i == len(month_days) - 1:  # 마지막 주
+                    month_days[i][j] = (box, 'other-month')
+                    box += 1
+            else:
+                month_days[i][j] = (day, 'current-month')
+
+    # 한국어 요일과 월을 설정
+    weekdays = ["월", "화", "수", "목", "금", "토", "일"]
+
+    selected_weekday_index = calendar.weekday(year, month, selected_day)
+    selected_weekday = weekdays[selected_weekday_index]
+
+    context = {
+        'year': year,
+        'month': month,
+        'selected_day': selected_day,
+        'cal_rows': month_days,
+        'weekdays': weekdays,
+        'prev_year': prev_year,
+        'prev_month': prev_month,
+        'next_year': next_year,
+        'next_month': next_month,
+        'selected_weekday': selected_weekday,
+    }
+
+    return render(request, 'cal/home2.html', context)
